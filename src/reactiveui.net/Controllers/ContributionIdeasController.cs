250a10ca5c6a3fe53f6dcc6880e412e669548a03
Geoffrey Huntley <ghuntley@ghuntley.com>
2017-05-20 20:01:52 +0800

    added reactiveui.net/contribution-ideas shortcut

---
+++ src/reactiveui.net/Controllers/ContributionIdeasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace reactiveui.net.Controllers
+{
+    public class ContributionIdeasController : Controller
+    {
+        [Route("/contribution-ideas")]
+        public IActionResult Index()
+        {
+            return Redirect("https://reactiveui.net/dashboard/?showOpen=true&showClosed=false&showCommented=true&showUncommented=true&showIssues=true&showPullRequests=false&last24Hours=false&repos=null&labels=null&milestones=null&usernames=null#reactiveui");
+        }
+    }
+}
