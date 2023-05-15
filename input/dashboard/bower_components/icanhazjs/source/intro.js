2bfe27436d62e5c3c01d6c58019a280bba1925f2
Glenn Watson <5834289+glennawatson@users.noreply.github.com>
2023-05-16 09:38:36 +1000

    Remove old dashboard

--- input/dashboard/bower_components/icanhazjs/source/intro.js
+++
@@ -1,4 +0,0 @@
-/*!
-ICanHaz.js version @VERSION@ -- by @HenrikJoreteg
-More info at: https://icanhazjs.com
-*/
--- input/dashboard/bower_components/reset-css/README.md
+++
@@ -1,4 +0,0 @@
-reset.css
-=========
-
-> An unmodified copy of Eric Meyer's CSS reset.
