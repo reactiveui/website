971cf63a9721e0540362d8992c1b166f47d772f3
Glenn Watson <5834289+glennawatson@users.noreply.github.com>
2023-05-27 15:41:32 +1000

    Add a source fetcher to do async fetching

--- Program.cs
+++ Program.cs
@@ -1,17 +1,8 @@
-﻿using Statiq.Markdown;
-using System.IO.Compression;
-
-Directory.CreateDirectory("external/Zip");
-await GetSource("reactiveui"); 
-await GetSource("akavache");
-await GetSource("fusillade");
-await GetSource("punchclock");
-await GetSource("splat");
-await GetSource("DynamicData", "reactivemarbles");
-
-await Bootstrapper
+﻿await Bootstrapper
         .Factory
         .CreateDocs(args)
+        .GetSources("reactiveui", "reactiveui", "akavache", "fusillade", "punchclock", "splat")
+        .GetSources("reactivemarbles", "DynamicData")
         .AddSetting(MarkdownKeys.MarkdownExtensions, "bootstrap")
         .AddSourceFiles(
             "../../dynamicdata/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
@@ -21,37 +12,3 @@ await Bootstrapper
             "../../punchclock/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
             "../../fusillade/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs")
         .RunAsync();
-
-static async Task GetSource(string repository, string owner = "reactiveui")
-{
-    var url = $"https://codeload.github.com/{owner}/{repository}/zip/main";
-    var zipPath = $"external/Zip/{repository}.zip";
-    File.Delete(zipPath);
-
-    using (HttpClient client = new HttpClient())
-    {
-        var response = await client.GetAsync(url);
-        response.EnsureSuccessStatusCode();
-        using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
-            stream = new FileStream(zipPath, FileMode.Create, FileAccess.Write, FileShare.None))
-        {
-            await contentStream.CopyToAsync(stream);
-        }
-    }
-
-    var extractPath = $"external/Zip/{repository}/";
-    var finalPath = $"external/{repository}";
-    ZipFile.ExtractToDirectory(zipPath, extractPath);
-    if (Directory.Exists(finalPath))
-    {
-        Directory.Delete(finalPath, true);
-    }
-
-    Directory.Move($"{extractPath}{repository}-main", finalPath);
-    if (Directory.Exists(extractPath))
-    {
-        Directory.Delete(extractPath, true);
-    }
-
-    File.Delete(zipPath);
-}
---
+++ SourceFetcher.cs
@@ -0,0 +1,76 @@
+﻿using System.IO.Compression;
+
+using Polly.RateLimit;
+using Polly;
+
+namespace ReactiveUI.Web;
+
+internal static class SourceFetcher
+{
+    public static Bootstrapper GetSources(this Bootstrapper bootStrapper, string owner, params string[] repositories)
+    {
+        Directory.CreateDirectory("external/Zip");
+
+        using var client = new HttpClient();
+
+        var waitAndRetry = Policy.Handle<Exception>()
+            .WaitAndRetryAsync(
+                6, // We can also do this with WaitAndRetryForever... but chose WaitAndRetry this time.
+                attempt => TimeSpan.FromSeconds(0.1 * Math.Pow(2,
+                                                    attempt))); // Back off!  2, 4, 8, 16 etc times 1/4-second
+
+        var rateLimitTime = TimeSpan.FromSeconds(1);
+
+        // Allow up to 3 executions per second.
+        var rateLimit = Policy.RateLimitAsync(3, rateLimitTime);
+
+        var policy = Policy.Handle<RateLimitRejectedException>()
+            .WaitAndRetryAsync(5, _ => rateLimitTime)
+            .WrapAsync(rateLimit);
+
+        Task.WaitAll(repositories.Select(repository => Task.Run(async () =>
+        {
+            var url = $"https://codeload.github.com/{owner}/{repository}/zip/main";
+
+            var zipPath = $"external/Zip/{repository}.zip";
+            var extractPath = $"external/Zip/{repository}/";
+            var finalPath = $"external/{repository}";
+
+            File.Delete(zipPath);
+
+            // Retry the following call according to the policy - 15 times.
+            await policy.ExecuteAsync(() => waitAndRetry.ExecuteAsync(async () =>
+            {
+                var response = await client.GetAsync(url);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    bootStrapper.FileSystem.GetCachePath(zipPath);
+                    throw new HttpRequestException("Could not find a valid document at: " + url, default, response.StatusCode);
+                }
+
+                using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                    stream = new FileStream(zipPath, System.IO.FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    await contentStream.CopyToAsync(stream);
+                }
+
+                ZipFile.ExtractToDirectory(zipPath, extractPath);
+                if (Directory.Exists(finalPath))
+                {
+                    Directory.Delete(finalPath, true);
+                }
+
+                Directory.Move($"{extractPath}{repository}-main", finalPath);
+                if (Directory.Exists(extractPath))
+                {
+                    Directory.Delete(extractPath, true);
+                }
+
+                File.Delete(zipPath);
+            }));
+        })).ToArray());
+
+        return bootStrapper;
+    }
+}
