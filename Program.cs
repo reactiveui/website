95211a4a2c47d10892834532b050d716fea94c69
Chris Pulman <chris.pulman@yahoo.com>
2023-05-27 04:25:25 +0100

    Updated filers for API sources, tested and generates output Splat.Drawing has an issue that needs resolving long term

--- Program.cs
+++ Program.cs
@@ -15,26 +15,11 @@ await Bootstrapper
         .AddSetting(MarkdownKeys.MarkdownExtensions, "bootstrap")
         .AddSourceFiles(
             "../../dynamicdata/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
-            ////"../../splat/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
-            "../../reactiveui/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,!*.TestRunner,!*.LeakTests,}/**/*.cs",
+            "../../splat/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.TestRunner,!Benchmarks,}/**/*.cs",
+            "../../reactiveui/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Test,!*.Uwp,!*.Templates,!Benchmarks,!*.TestRunner,!*.LeakTests,}/**/*.cs",
             "../../akavache/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
             "../../punchclock/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
             "../../fusillade/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs")
-        .AddProjectFiles(
-            "../../splat/src/Splat/Splat.csproj",
-            //// TODO FIND ISSUE WITH LOADING THIS"../../splat/src/Splat.Drawing/Splat.Drawing.csproj",
-            "../../splat/src/Splat.ApplicationInsights/Splat.ApplicationInsights.csproj",
-            "../../splat/src/Splat.Autofac/Splat.Autofac.csproj",
-            "../../splat/src/Splat.Exceptionless/Splat.Exceptionless.csproj",
-            "../../splat/src/Splat.Log4Net/Splat.Log4Net.csproj",
-            "../../splat/src/Splat.Microsoft.Extensions.DependencyInjection/Splat.Microsoft.Extensions.DependencyInjection.csproj",
-            "../../splat/src/Splat.Microsoft.Extensions.Logging/Splat.Microsoft.Extensions.Logging.csproj",
-            "../../splat/src/Splat.Ninject/Splat.Ninject.csproj",
-            "../../splat/src/Splat.NLog/Splat.NLog.csproj",
-            "../../splat/src/Splat.Prism/Splat.Prism.csproj",
-            "../../splat/src/Splat.Raygun/Splat.Raygun.csproj",
-            "../../splat/src/Splat.Serilog/Splat.Serilog.csproj",
-            "../../splat/src/Splat.SimpleInjector/Splat.SimpleInjector.csproj")
         .RunAsync();
 
 static async Task GetSource(string repository, string owner = "reactiveui")
