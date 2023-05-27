75decbbf22a694ee30bc05350faf5dd16103159c
Chris Pulman <chris.pulman@yahoo.com>
2023-05-27 01:33:13 +0100

    add downloader

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿using Statiq.Markdown;
+using System.IO.Compression;
+
+Directory.CreateDirectory("external/Zip");
+await GetSource("reactiveui"); 
+await GetSource("akavache");
+await GetSource("fusillade");
+await GetSource("punchclock");
+await GetSource("splat");
+await GetSource("DynamicData", "reactivemarbles");
 
 await Bootstrapper
         .Factory
@@ -7,7 +16,7 @@ await Bootstrapper
         .AddSourceFiles(
             "../../dynamicdata/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
             ////"../../splat/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
-            //// TODO: OBTAIN SOURCE"../../reactiveui/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
+            "../../reactiveui/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,!*.TestRunner,!*.LeakTests,}/**/*.cs",
             "../../akavache/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
             "../../punchclock/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs",
             "../../fusillade/src/**/{!.git,!bin,!obj,!packages,!*.Tests,!*.Templates,!*.Benchmarks,}/**/*.cs")
@@ -27,3 +36,37 @@ await Bootstrapper
             "../../splat/src/Splat.Serilog/Splat.Serilog.csproj",
             "../../splat/src/Splat.SimpleInjector/Splat.SimpleInjector.csproj")
         .RunAsync();
+
+static async Task GetSource(string repository, string owner = "reactiveui")
+{
+    var url = $"https://codeload.github.com/{owner}/{repository}/zip/main";
+    var zipPath = $"external/Zip/{repository}.zip";
+    File.Delete(zipPath);
+
+    using (HttpClient client = new HttpClient())
+    {
+        var response = await client.GetAsync(url);
+        response.EnsureSuccessStatusCode();
+        using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+            stream = new FileStream(zipPath, FileMode.Create, FileAccess.Write, FileShare.None))
+        {
+            await contentStream.CopyToAsync(stream);
+        }
+    }
+
+    var extractPath = $"external/Zip/{repository}/";
+    var finalPath = $"external/{repository}";
+    ZipFile.ExtractToDirectory(zipPath, extractPath);
+    if (Directory.Exists(finalPath))
+    {
+        Directory.Delete(finalPath, true);
+    }
+
+    Directory.Move($"{extractPath}{repository}-main", finalPath);
+    if (Directory.Exists(extractPath))
+    {
+        Directory.Delete(extractPath, true);
+    }
+
+    File.Delete(zipPath);
+}
