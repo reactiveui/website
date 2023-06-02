4f3e6846c7d76f13c51f7b4d934c31b3f828cf33
Chris Pulman <chris.pulman@yahoo.com>
2023-06-03 00:02:11 +0100

    Revert "Run Test to confirm output generation"
    This reverts commit e6f88dcf3c876c929f929c63828675e8579332ef.

--- Program.cs
+++ Program.cs
@@ -2,35 +2,15 @@
         .Factory
         .CreateDocs(args)
         .FetchTheme()
-        //.GetSources("reactiveui", "reactiveui", "akavache", "fusillade", "punchclock", "splat")
-        //.GetSources("reactivemarbles", "DynamicData")
+        .GetSources("reactiveui", "reactiveui", "akavache", "fusillade", "punchclock", "splat")
+        .GetSources("reactivemarbles", "DynamicData")
         .ConfigureLinks(args)
         .AddSetting(MarkdownKeys.MarkdownExtensions, "bootstrap")
-        //.AddSourceFiles(
-        //    "dynamicdata".WithSourceFilter(),
-        //    "splat".WithSourceFilter("*.Drawing"),
-        //    "reactiveui".WithSourceFilter("Benchmarks", "*.Test", "*.LeakTests", "*.TestRunner", "*.Uwp"),
-        //    "akavache".WithSourceFilter(),
-        //    "punchclock".WithSourceFilter(),
-        //    "fusillade".WithSourceFilter())
+        .AddSourceFiles(
+            "dynamicdata".WithSourceFilter(),
+            "splat".WithSourceFilter("*.Drawing"),
+            "reactiveui".WithSourceFilter("Benchmarks", "*.Test", "*.LeakTests", "*.TestRunner", "*.Uwp"),
+            "akavache".WithSourceFilter(),
+            "punchclock".WithSourceFilter(),
+            "fusillade".WithSourceFilter())
         .RunAsync();
-
-var entries = Directory.GetFileSystemEntries("output", "*", SearchOption.AllDirectories);
-foreach (var entry in entries)
-{
-    Console.WriteLine(entry);
-}
-
-StreamReader sr = new StreamReader("output\\index.html");
-//Read the first line of text
-var line = sr.ReadLine();
-//Continue to read until you reach end of file
-while (line != null)
-{
-    //write the line to console window
-    Console.WriteLine(line);
-    //Read the next line
-    line = sr.ReadLine();
-}
-//close the file
-sr.Close();
